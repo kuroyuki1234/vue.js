d315f19c1b546de52c476fba1d21e15a9c9afb13
kuroyuki1234 <umetsu-t@smhc.co.jp>
2025-04-08 13:32:29 +0900

    修正

--- kensyu/src/components/Validation.js
+++ kensyu/src/components/Validation.js
@@ -1,7 +1,71 @@
-export function validationForm(submitForm) {
+export function validationForm(form) {
     let errors = {}
-    if (!submitForm.sei) errors.sei = "姓は必須です"
-    if (!submitForm.mei) errors.mei = "名は必須です"
+//　名字確認
+    if (!form.sei) {
+        errors.sei = "姓は必須です、入力してください"
+    } else if (form.sei.length < 1) {
+        errors.sei = "姓は1文字以上で入力してください"
+    }
+// 名前確認
+    if (!form.mei) {
+        errors.mei = "名は必須です、入力してください"
+    }　else if (form.mei.length < 1) {
+        errors.mei = "名は1文字以上で入力してください"
+    }
+// 名字のカナ確認
+    if (!form.seikana) {
+        errors.seikana = "セイは必須です、入力してください"
+    } else if (form.seikana.length < 1) {
+        errors.seikana = "セイは1文字以上で入力してください"
+    }
+// 名前のカナ確認
+    if (!form.meikana) {
+        errors.meikana = "メイは必須です、入力してください"
+    } else if (form.meikana.length < 1) {
+        errors.meikana = "メイは1文字以上で入力してください"
+    }
+//メールの確認
+    if (!form.email) {
+        errors.email = "メールアドレスは必須です、入力してください"
+    } else if (form.email.length < 1) {
+        errors.email = "メールアドレスは1文字以上で入力してください"
+    }
+//パスワードの確認
+    if (!form.password1) {
+        errors.password = "パスワードは必須です、入力してください"
+    } else if (form.password.length < 1) {
+        errors.password = "パスワードは1文字以上で入力してください"
+    }
+//パスワード再確認
+    if (!form.passwordConfirm) {
+        errors.passwordConfirm = "パスワード確認は必須です、入力してください"
+    } else if (form.password !== form.passwordConfirm) {
+        errors.passwordConfirm = "パスワードが一致しません"
+    }
+//郵便番号の確認
+    if (!form.postalCode) {
+        errors.postalCode = "郵便番号は必須です、入力してください"
+    } else if (form.postalCode.length < 1) {
+        errors.postalCode = "郵便番号は1文字以上で入力してください"
+    }
+//住所の確認
+    if (!form.address1) {
+        errors.address1 = "住所は必須です、入力してください"
+    } else if (form.address.length < 1) {
+        errors.address1 = "住所は1文字以上で入力してください"
+    }
+//電話番号(自宅)の確認
+    if (!form.homeTel) {
+        errors.homeTel = "自宅番号は必須です"
+    } else if (form.homeTel.length < 1) {
+        errors.homeTel = "自宅番号は1文字以上で入力してください"
+    }
+//電話番号(携帯)の確認
+    if (!form.tel) {
+        errors.tel = "携帯番号は必須です"
+    } else if (form.tel.length < 1) {
+        errors.tel = "携帯番号は1文字以上で入力してください"
+    }
 
     return errors
 }
--- kensyu/src/components/registration.vue
+++ kensyu/src/components/registration.vue
@@ -4,72 +4,52 @@ import { useRouter } from 'vue-router'
 import Completion from './Completion.vue';
 import { validationForm } from './Validation.js';
 
-
 const router = useRouter();
+const errors = reactive({});
 
-// const form = ref({
-//     sei: "",
-//     mei: "",
-//     email: "",
-//     password: "",
-//     seikana: "",
-//     meikana: "",
-//     postcode: "",
-//     address: "",
-//     number: "",
-// });
-const form = reactive({
+const form = reactive ({
     sei: "",
     mei: "",
-    email: "",
-    password: "",
     seikana: "",
     meikana: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
     postcode: "",
-    address: "",
-    number: "",
+    address1: "",
+    address2: "",
+    homeTel: "",
+    tel: "",
+
 });
 
-const errors = reactive({});
 
 
 const validateForm = () => {
-    Object.assing(errors, validateForm(form));
-
-    if (Object.keys(errors).length === 0) {
-        console.log("送信成功" , form);
-    }else {
-        console.log("送信失敗", errors);
-    };
-
     console.log("Form submitted:", form);
-
-    // Object.assign(errors, {
-    //     sei: form.sei.length < 3 ? "名前は3文字以上必要です" : "",
-    //     mei: form.mei.length < 3 ? "名前は3文字以上必要です" : "",
-    //     seikana: form.seikana.length < 3 ? "名前は3文字以上必要です" : "",
-    //     meikana: form.meikana.length < 3 ? "名前は3文字以上必要です" : "",
-    //     email: !form.email.includes("@") ? "メールアドレスが無効です" : "",
-    //     password: form.password.length < 6 || form.password.length > 12 ? "パスワードは6~12文字である必要があります" : "",
-    //     postcode: form.postcode.length < 7 ? "郵便番号は7桁である必要があります" : "",
-    //     address: form.address.length < 5 ? "住所は5文字以上必要です" : "",
-    //     number: form.number.length < 10 ? "電話番号は10桁以上必要です" : "",
-    // });
-    // Add your form submission logic here
 };
 
 const completion = () => {
     const hasErrors = validateForm();
-    if (!isValid) {
-        console.log("送信成功", form);
-    } else {
-        console.log("送信失敗", errors);
-    }
-    // router.push({ name: 'Completion' });
+    // if (!isValid) {
+    //     console.log("送信成功", form);
+    // } else {
+    //     console.log("送信失敗", errors);
+    // }
+    router.push({ name: 'Completion' });
 }
 
-const handleEnter = () => {
-    console.log("送信中")
+// エンター機を押した際の処理を記載(対象の項目についても何をするのかを記載すること)
+// const handleEnter = () => {
+//     console.log("送信中")
+// }
+
+function validateField(fieldName) {
+    console.log('Validating field:', fieldName)
+    console.log('submitForm:', form)
+    const allErrors = validationForm(form);
+    errors[fieldName] = allErrors[fieldName] || ''
+    console.log('errors:', errors)
 }
 
 </script>
@@ -82,43 +62,54 @@ const handleEnter = () => {
         <form @submit.prevent="submitForm">
             <div class="form-group">
                 <label for="sei">姓：</label>
-                <input type="text" id="sei" v-model="form.sei" placeholder="山田" @keyup.enter="handleEnter"required />
+                <!-- <input type="text" id="sei" v-model="form.sei" @blur="() => validateField('sei')" placeholder="山田" @keyup.enter="handleEnter" required /> -->
+                <input type="text" id="sei" v-model="form.sei"required @blur="() => validateField('sei')"  maxlength="6" placeholder="山田" />
                 <p v-if="errors.sei" class="error"> {{ errors.sei }} </p>
                 <label for="mei">名：</label>
-                <input type="text" id="mei" v-model="form.mei" placeholder="太郎" required />
+                <input type="text" id="mei" v-model="form.mei"required @blur="() => validateField('mei')" placeholder="太郎" />
+                <p v-if="errors.mei" class="error"> {{ errors.mei }} </p>
             </div>
             <div class="form-group">
                 <label for="seikana">セイ：</label>
-                <input type="text" id="seikana" v-model="form.seikana" placeholder="ヤマダ" required />
+                <input type="text" id="seikana" v-model="form.seikana" required @blur="() => validateField('seikana')" maxlength="6" placeholder="ヤマダ" />
+                <p v-if="errors.seikana" class="error"> {{ errors.seikana }} </p>
                 <label for="meikana">メイ：</label>
-                <input type="text" id="meikana" v-model="form.meikana" placeholder="タロウ" required />
+                <input type="text" id="meikana" v-model="form.meikana" required @blur="() => validateField('meikana')" placeholder="タロウ" />
+                <p v-if="errors.meikana" class="error"> {{ errors.meikana }} </p>
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" v-model="form.email" placeholder="agobu@gmail.com" required />
+                <input type="email" id="email" v-model="form.email" required @blur="() => validateField('email')" placeholder="agobu@gmail.com" />
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" v-model="form.password" required minilength="6" maxlength="12" placeholder="6~12文字のPW"/>
+                <input type="password" id="password1" v-model="form.password1" required @blur="() => validateField('password')" minlength="6" maxlength="12" placeholder="6~12文字のPW"/>
+            </div>
+            <div class="form-group">
+                <label for="password">(再確認用)Password:</label>
+                <input type="password" id="passwordConfirm" v-model="form.passwordConfirm" required @blur="() => validateField('passwordConfirm')" minilength="6" maxlength="12" placeholder="6~12文字のPW"/>
+                <p v-if="errors.passwordConfirm" class="error"> {{ errors.passwordConfirm }} </p>
             </div>
             <div class="form-group">
                 <label for="postcode">郵便番号：</label>
-                <input type="text" id="postcode" v-model="form.postcode" placeholder="160-0023" required />
+                <input type="text" id="postcode" v-model="form.postcode" required @blur="() => validateField('postcode')" placeholder="160-0023" />
             </div>
             <div class="form-group">
                 <label for="address">住所：</label>
-                <input type="text" id="address" v-model="form.address" placeholder="東京都新宿区西新宿7-21-1" required />
+                <input type="text" id="address1" v-model="form.address1" required @blur="() => validateField('address1')" placeholder="東京都新宿区西新宿7-21-1" />
             </div>
             <div class="form-group">
                 <label for="address2">ビル名や部屋番号</label>
-                <input type="text" id="address2" v-model="form.address" placeholder="新宿ロイヤルビル201" required />
+                <input type="text" id="address2" v-model="form.address2" @blur="() => validateField('address2')" placeholder="新宿ロイヤルビル201" />
             </div>
             <div class="form-group">
-                <label for="number">電話番号：</label>
-                <input type="text" id="number" v-model="form.number" placeholder="0369086228" required />
+                <label for="number">電話番号(自宅)：</label>
+                <input type="tel" id="tel1" v-model="form.homeTel" required @blur="() => validateField('homeTel')" placeholder="0369086228"  />
             </div>
             <div class="form-group">
-                
+                <label for="number">電話番号(携帯)：</label>
+                <input type="tel" id="tel2" v-model="form.tel" required @blur="() => validateField('tel')" placeholder="08069086228" />
+                <p v-if="errors.tel" class="error"> {{ errors.tel }} </p>
             </div>
             <div class="form-group">
                 
@@ -132,23 +123,27 @@ const handleEnter = () => {
 .registration {
     max-width: Auto;
     margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
+    padding: 0 auto;
+    /* border: 1px solid #ccc; */
     border-radius: 5px;
 }
 .form-group {
-    margin-bottom: 15px;
+    margin-bottom: 20px;
     display: flex;
     align-items: center;
-    gap: 10px;
+    gap: 30px;
 }
 label {
-    display: block;
+    display: inline-block;
+    width: 100px;
+    font-weight: bold;
+    color: #333;
     margin-bottom: 5px;
+    white-space: nowrap;
 }
 input {
     width: 100%;
-    padding: 8px;
+    padding: 10px;
     box-sizing: border-box;
 }
 button {
